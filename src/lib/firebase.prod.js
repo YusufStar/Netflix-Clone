8228b2419d816bb0264e923b0fba1fe5e12f0fb4
yusuf <107367817+YusufStar@users.noreply.github.com>
2022-10-07 14:04:21 +0300

    Update firebase.prod.js

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -1,6 +1,7 @@
 import Firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
+import { seedDatabase } from "../seed"
 
 // 1) when seeding the database you'll have to uncomment this!
 // import { seedDatabase } from '../seed';
@@ -16,8 +17,7 @@ const config = {
 };
 
 const firebase = Firebase.initializeApp(config);
-// 2) when seeding the database you'll have to uncomment this!
+
 // seedDatabase(firebase);
-// 3) once you have populated the database (only run once!), re-comment this so you don't get duplicate data
 
 export { firebase };
